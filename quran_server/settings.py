f758a6794cea74e714eabe92badf444473593222
Iqbal Syifa Mahmuda <bal.mahmuda@gmail.com>
2023-01-08 20:34:12 +0700

    Added `TEMPLATES` `DIRS`
    that use `BASE_DIR`

--- quran_server/settings.py
+++ quran_server/settings.py
@@ -74,7 +74,9 @@ ROOT_URLCONF = "quran_server.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [
+            BASE_DIR / "templates",
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
