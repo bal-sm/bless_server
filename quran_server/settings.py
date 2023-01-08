bf34878836e9803fee041acb8526b19e9608c749
Iqbal Syifa Mahmuda <bal.mahmuda@gmail.com>
2023-01-08 11:41:42 +0700

    Allowed all hosts

--- quran_server/settings.py
+++ quran_server/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DJANGO_DEBUG", True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
