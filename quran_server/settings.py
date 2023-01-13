b18ec3af2067d40d9c2cc9055c9be07be505a0e4
Iqbal Syifa Mahmuda <bal.mahmuda@gmail.com>
2023-01-13 14:48:40 +0700

    Added `django_htmx.middleware.HtmxMiddleware` to `MIDDLEWARE`

--- quran_server/settings.py
+++ quran_server/settings.py
@@ -71,6 +71,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django_htmx.middleware.HtmxMiddleware",
 ]
 
 ROOT_URLCONF = "quran_server.urls"
