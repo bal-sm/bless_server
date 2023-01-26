f593f97983b6c5aef9b7109c389bf152b9ba4915
Iqbal Syifa Mahmuda <bal.mahmuda@gmail.com>
2023-01-27 05:54:25 +0700

    Added `REST_FRAMEWORK` settings

--- quran_server/settings.py
+++ quran_server/settings.py
@@ -161,3 +161,12 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# REST_FRAMEWORK settings
+# https://www.django-rest-framework.org/api-guide/settings/
+
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
+}
