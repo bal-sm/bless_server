67705e6c2460a6c523e5e614a48c0b1c66734c60
Iqbal Syifa Mahmuda <bal.mahmuda@gmail.com>
2023-01-27 06:30:49 +0700

    Wired up `QuranViewSet` to `urls.py`
    asik

--- dquran/urls.py
+++ dquran/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
+from rest_framework import routers
 
 from . import views
+
+router = routers.DefaultRouter()
+router.register(r"quran", views.QuranViewSet)
 
 urlpatterns = [
     path("list/", views.quran_list, name="quran_list"),
