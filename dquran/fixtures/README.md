06a9fce272eee2cb7b67be50b107ef69cafbfaf2
Imam Mahdi <bal.mahmuda@gmail.com>
2023-02-21 12:56:32 +0700

    Deleted `dquran` app
    From this repository.

--- dquran/fixtures/README.md
+++
@@ -1,3 +0,0 @@
-# dakun fixtures
-## Load Qur'an Surat An-Nas fixture
-    python manage.py loaddata dquran.An-Nas.yaml
--- dquran/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from .models import Quran
-
-
-class QuranSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Quran
-        fields = ["surat", "ayat", "text"]
