16c574195ce98470715921242d58befa518e3769
Iqbal Syifa Mahmuda <bal.mahmuda@gmail.com>
2023-02-22 20:10:41 +0700

    Renamed `quran_server` folder to `bless_server`
    and all previous `quran_server` references in those `.py` files are edited also.

--- bless_server/asgi.py
+++ bless_server/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for quran_server project.
+ASGI config for bless_server project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "quran_server.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bless_server.settings")
 
 application = get_asgi_application()
