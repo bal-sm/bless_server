7326e1ffac6f4763dd17864de80e34dd9aad3a81
Iqbal Syifa Mahmuda <bal_mahmuda@protonmail.com>
2023-02-23 18:58:57 +0700

    Removed `TranslationVersion` and `AyatTranslation` model
    and both of its admin for the meantime.

--- dquran/admin.py
+++ dquran/admin.py
@@ -1,13 +1,1 @@
 from django.contrib import admin
-
-from .models import AyatTranslation
-from .models import TranslationVersion
-
-
-class TranslationInline(admin.StackedInline):
-    model = AyatTranslation
-
-
-@admin.register(TranslationVersion)
-class TranslationVersionAdmin(admin.ModelAdmin):
-    pass
--- dquran/models.py
+++ dquran/models.py
@@ -63,29 +63,3 @@ class Ayatship(AbstractAyatship):
 
 class Ayat(AbstractAyat):
     pass
-
-
-class TranslationVersion(models.Model):
-    version = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.version
-
-    class Meta:
-        ordering = ["version"]
-        verbose_name_plural = "Translation Versions"
-
-
-class AyatTranslation(models.Model):
-    ayat = models.ForeignKey(Ayat, on_delete=models.CASCADE)
-    translation_version = models.ForeignKey(
-        TranslationVersion, on_delete=models.CASCADE
-    )
-    translation = models.TextField()
-
-    def __str__(self):
-        return f"{self.quran}, {self.translation_version}"
-
-    class Meta:
-        ordering = ["ayat", "translation_version"]
-        verbose_name_plural = "Translations"
