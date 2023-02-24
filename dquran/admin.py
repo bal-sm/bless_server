4ce6ec89a865ee69721354f3e2301cbb31992f97
Iqbal Syifa Mahmuda <bal_mahmuda@protonmail.com>
2023-02-24 14:36:36 +0700

    Created simple `admin`s for existing `models.py` of `dquran`

--- dquran/admin.py
+++ dquran/admin.py
@@ -1,1 +1,20 @@
 from django.contrib import admin
+
+from .models import Ayat
+from .models import Ayatship
+from .models import Surat
+
+
+@admin.register(Surat)
+class SuratAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Ayatship)
+class AyatshipAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Ayat)
+class AyatAdmin(admin.ModelAdmin):
+    pass
