7e3190a597259f390e19b811989fd936f6dcb60e
Iqbal Syifa Mahmuda <bal_mahmuda@protonmail.com>
2023-02-26 13:49:54 +0700

    Restored `urls.py`, `views.py`, and their related htmls
    associated with deletion of non-functional `Django`-based codes in f173480.

--- dquran/urls.py
+++ dquran/urls.py
@@ -5,4 +5,8 @@ from . import views
 
 router = routers.DefaultRouter()
 
-urlpatterns = []
+urlpatterns = [
+    path("", views.surat_list, name="surat_list"),
+    path("<int:surat_number>", views.ayatship_list, name="ayatship_list"),
+    path("<int:surat_number>/<int:ayat_number>", views.ayat_view, name="ayat_view"),
+]
--- dquran/views.py
+++ dquran/views.py
@@ -1,3 +1,35 @@
+from django.shortcuts import get_list_or_404
 from django.shortcuts import get_object_or_404
 from django.shortcuts import render
-from rest_framework import viewsets
+
+from .models import Ayatship
+from .models import Surat
+
+
+def surat_list(request):
+    root = Surat.objects.all()
+    context = {
+        "root": root,
+    }
+    return render(request, "quran/surat_list.html", context)
+
+
+def ayatship_list(request, surat_number):
+    root = get_list_or_404(Ayatship, surat__number=surat_number)
+    context = {
+        "root": root,
+    }
+    return render(request, "quran/ayatship_list.html", context)
+
+
+def ayat_view(request, surat_number, ayat_number):
+    ayatship = get_object_or_404(
+        Ayatship,
+        surat__number=surat_number,
+        number=ayat_number,
+    )
+
+    context = {
+        "ayatship": ayatship,
+    }
+    return render(request, "quran/ayat_view.html", context)
